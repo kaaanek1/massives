dd46d1897f636991493330b41d57e3d14d2cc372
Taya <151873595+kaaanek1@users.noreply.github.com>
2024-01-26 20:22:26 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,129 @@
+﻿//ОДНОМЕРНЫЕ МАССИВЫ
+using System;
+
+
+namespace _17._01._24
+{
+   class Program
+   {
+        static void Main()
+        {
+            Console.WriteLine("Введи длину массива:");
+            int len = int.Parse(Console.ReadLine());
+            Console.WriteLine("Будешь заполнять массив сам?(да/нет)");
+            string str = Console.ReadLine();
+            bool fill;
+            if (str=="да"){
+                fill = true;
+            }
+            else{
+                fill = false;
+            }
+            LVL1 array_1 = new LVL1(len, fill);
+            Console.WriteLine("Исходный массив:");
+            array_1.Print(array_1.array);
+            Console.WriteLine("Среднее арифметическое:");
+            array_1.Average();
+            Console.WriteLine("Значения меньше ста по модулю:");
+            array_1.Delete100();
+            Console.WriteLine("Только уникальные элементы:");
+            array_1.Uniqal();
+        }
+    }
+
+
+   class LVL1
+   {
+       public int[] array;
+
+       private int len;
+      
+       public LVL1(int l, bool f){
+           len = l;
+           array = new int[l];
+           if (!f)
+           {
+               Random rnd = new Random();
+               for (int i = 0; i<l; i++){
+                   array[i] = rnd.Next(-1000, 1000);
+               }
+           }
+
+           else
+           {
+               for (int i = 0; i<l; i++){
+                   Console.Write($"Элемент {i}: ");
+                   int x = int.Parse(Console.ReadLine());
+                   array[i] = x;
+               }
+           }
+       }
+      
+      
+       public void Print(int[] arr)
+       {
+           int l1 = arr.Length;
+           for (int i = 0; i<l1; i++)
+           {
+               if (i==l1-1) {Console.WriteLine($"{arr[i]}");}
+               else {Console.Write($"{arr[i]} ");}
+           }
+       }
+      
+      
+       public void Average()
+       {
+           int sum = 0;
+           foreach (int el in array)
+           {
+               sum += el;
+           }
+           Console.WriteLine(sum/len);
+       }
+      
+      
+       public void Delete100()
+       {
+           int new_len = 0;
+           int i = 0;
+           foreach (int el in array)
+           {
+               if (Math.Abs(el)<=100)
+               {
+                   new_len ++;
+               }
+           }
+           int[] new_arr = new int[new_len];
+           foreach (int l in array)
+           {
+               if (Math.Abs(l)<100)
+               {
+                   new_arr[i] = l;
+                   i ++;
+               }
+           }
+           Print(new_arr);
+       }
+      
+      
+       public void Uniqal(){
+           int[] newArray = new int[len];
+           int uniq = 1;
+           for (int i = 0; i < len; i++) {
+               bool isUniq = true;
+               for (int j = i + 1; j < len; j++) {
+                   if (array[i] == array[j]) {
+                       isUniq = false;
+                   }
+               }
+               if (isUniq) {
+                   newArray[uniq - 1] = array[i];
+                   uniq++;
+               }
+           }
+           Print(newArray);
+       }
+   }
+}
+
+
---
+++ Program3.cs
@@ -0,0 +1,159 @@
+﻿//СТУПЕНЧАТЫЕ МАССИВЫ
+using System;
+
+
+namespace _17._01._24
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+            Console.WriteLine("Введите длину ступенчатого массива:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Будешь заполнять массив сам?(да/нет)");
+            string s = Console.ReadLine();
+            bool fill;
+            if (s == "да") {fill = true;}
+            else {fill = false;}
+            LVL3 array_3 = new LVL3(a, fill);
+            Console.WriteLine("Создан массив вида:");
+            array_3.Print(array_3.array);
+            array_3.global_average();
+            array_3.local_average();
+            Console.WriteLine("Массив с измененными четными значениями:");
+            array_3.SwapOnInd();
+       }
+   }
+
+
+   class LVL3
+   {
+       private int a;
+       public int[][] array;
+      
+       public LVL3(int a1, bool f)
+       {
+           a = a1;
+           array = new int[a1][];
+
+           if (!f)
+           {
+               Random rnd = new Random();
+               for (int i = 0; i<a1; i++)
+               {
+                   Console.Write($"Длина {i} массива: ");
+                   int l = int.Parse(Console.ReadLine());
+                   array[i] = new int[l];
+                   for (int j = 0; j<l; j++)
+                   {
+                       int x = rnd.Next(-1000, 1000);
+                       array[i][j] = x;
+                   }
+               }
+           }
+
+           else
+           {
+               for (int i = 0; i<a1; i++)
+               {
+                   Console.Write($"Длина {i} массива: ");
+                   int l = int.Parse(Console.ReadLine());
+                   array[i] = new int[l];
+                   for (int j = 0; j<l; j++)
+                   {
+                       Console.Write($"Элемент [{i},{j}]: ");
+                       int x = int.Parse(Console.ReadLine());
+                       array[i][j] = x;
+                   }
+               }
+           }
+       }
+
+
+
+
+       public void global_average()
+       {
+           int sum = 0;
+           int el = 0;
+           for (int i = 0; i<a; i++)
+           {
+               for (int j = 0; j<array[i].Length; j++)
+               {
+                   int x = array[i][j];
+                   sum += x;
+                   el ++;
+               }
+           }
+           Console.WriteLine($"Среднее арифметическое целого массива: {sum/el}");
+       }
+
+
+
+
+       public void local_average()
+       {
+           int sum = 0;
+           int el = 0;
+           for (int i = 0; i<a; i++)
+           {
+               int[] arr = array[i];
+               foreach (int x in arr)
+               {
+                   sum += x;
+                   el ++;
+               }
+               Console.WriteLine($"Ср.арифм. [{i}] подмассива: {sum/el}");
+               sum = 0;
+               el = 0;
+           }
+       }
+
+
+
+
+       public void Print(int[][] arr)
+       {
+           for (int i = 0; i<a; i++)
+           {
+               for (int j = 0; j<arr[i].Length; j++)
+               {
+                   if(j==arr[i].Length-1)
+                   {
+                       Console.WriteLine(arr[i][j]);
+                   }
+
+
+                   else
+                   {
+                       Console.Write($"{arr[i][j]} ");
+                   }
+               }
+           }
+       }
+
+
+       public void SwapOnInd()
+       {
+            int[][] arr = new int[a][];
+            for (int i = 0; i<a; i++)
+            {
+                arr[i] = new int[array[i].Length];
+                for(int j = 0; j<array[i].Length; j++)
+                {
+                    int x = array[i][j];
+                    if (x%2==0)
+                    {
+                        arr[i][j] = i*j;
+                    }
+
+                    else
+                    {
+                        arr[i][j] = x;
+                    }
+                }
+            }
+            Print(arr);
+       }
+   }
+}
